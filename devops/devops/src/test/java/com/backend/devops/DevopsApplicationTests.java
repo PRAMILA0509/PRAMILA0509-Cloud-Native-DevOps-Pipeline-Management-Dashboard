9c405f3c550b532c8800657ca4909266dd0e957c
PRAMILA0509 <pramilavijayamani@gmail.com>
2025-12-19 10:55:05 +0530

    Add CI-friendly Spring Boot test with @ActiveProfiles and mocked external services

--- devops/devops/src/test/java/com/backend/devops/DevopsApplicationTests.java
+++ devops/devops/src/test/java/com/backend/devops/DevopsApplicationTests.java
@@ -1,13 +1,24 @@
 package com.backend.devops;
 
+import com.backend.devops.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@ActiveProfiles("test")
 class DevopsApplicationTests {
+
+	@MockBean
+	private UserRepository userRepository;  // if your service uses it
+
+//	@MockBean
+//	private KafkaProducer kafkaProducer;    // if your service uses it
 
 	@Test
 	void contextLoads() {
 	}
-
 }
+
